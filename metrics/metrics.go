364b99c9abfd176478c2ba35b43b67231af1caee
osamikoyo <v.pylayev@mail.ru>
2025-10-26 09:50:22 +0500

    add metrics

---
+++ metrics/metrics.go
@@ -0,0 +1,36 @@
+// mettics add measuaring functionality
+package metrics
+
+import (
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	// RequestTotal stores number of all request
+	RequestTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "request_total",
+			Help: "Total number of requests",
+		},
+		[]string{"path"},
+	)
+
+	// RequestDuration stores time for work with 1 request
+	RequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "request_duration_seconds",
+			Help:    "Duration of request",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"path"},
+	)
+)
+
+// InitMetrics() initialize metrics
+func InitMetrics() {
+	sync.OnceFunc(func() {
+		prometheus.MustRegister(RequestDuration, RequestTotal)
+	})()
+}
