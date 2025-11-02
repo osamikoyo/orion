0597a394130a987e725c1d13ed0d173f3960a94d
osamikoyo <v.pylayev@mail.ru>
2025-11-02 09:48:09 +0500

    add server

--- server/server.go
+++ server/server.go
@@ -2,85 +2,110 @@ package server
 
 import (
 	"context"
-	"time"
+	"fmt"
+	"net/http"
 
+	txhttp "github.com/corazawaf/coraza/v3/http"
 	"github.com/go-chi/chi/v5"
-	"github.com/osamikoyo/orion/auth"
-	"github.com/osamikoyo/orion/cache"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/osamikoyo/orion/config"
+	"github.com/osamikoyo/orion/handler"
+	"github.com/osamikoyo/orion/loadbalancer"
+	"github.com/osamikoyo/orion/logger"
 	"github.com/osamikoyo/orion/proxy"
-	"github.com/osamikoyo/orion/rate"
-	"github.com/osamikoyo/orion/selfcach"
-	"github.com/quic-go/quic-go/http3"
-	"github.com/rs/zerolog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
 )
 
-type Server struct {
-	router chi.Router
-	logger *zerolog.Logger
-	cfg    *config.Config
-	h3S    *http3.Server
-}
+const (
+	HttpProto  = "http"
+	Http3Proto = "http3"
+)
 
-func NewServer(r chi.Router, logger *zerolog.Logger, cfg *config.Config) *Server {
-	cache := cache.NewCache(
-		selfcach.NewCache(10*time.Minute, 5*time.Hour),
-		logger,
-		cfg,
-	)
+type (
+	Server interface {
+		Run() error
+		SetHandler(r chi.Router)
+		Shutdown(context.Context) error
+	}
+)
 
-	auth := auth.NewAuthMW(&cfg.AuthConfig, logger)
-	rl := rate.NewRateLimitingMiddleware(logger, &cfg.RateLimitingConfig)
+func NewServer(r *chi.Mux, logger *logger.Logger, cfg *config.Config) (Server, context.CancelFunc, error) {
+	loadbalancer, cancel, err := loadbalancer.NewLoadBalancer(cfg, logger)
+	if err != nil {
+		logger.Error("failed create load balancer", zap.Error(err))
+
+		return nil, nil, err
+	}
+
 	proxy := proxy.NewProxyMW(logger)
 
-	r.Route("/api", func(r chi.Router) {
-		for _, gateway := range cfg.Gateways {
-			route := r
+	handler := handler.NewHandler(proxy, loadbalancer, logger, cfg)
 
-			if gateway.Auth {
-				route = route.With(auth.Middleware)
-			}
+	if err := setupRouters(r, handler, cfg, logger); err != nil {
+		logger.Error("failed setup routers", zap.Error(err))
 
-			if gateway.Cash {
-				route = route.With(cache.Middleware)
-			}
+		return nil, nil, err
+	}
 
-			route.HandleFunc(gateway.Prefix+"/*", proxy.Middleware(gateway.Target))
-		}
+	server, err := setupServer(cfg, logger)
+	if err != nil {
+		logger.Error("failed to setup server",
+			zap.Error(err))
+
+		return nil, nil, err
+	}
+
+	server.SetHandler(r)
+
+	return server, cancel, nil
+}
+
+func setupRouters(r *chi.Mux, handler *handler.Handler, cfg *config.Config, logger *logger.Logger) error {
+	if cfg.CORS.Use {
+		r.Use(cors.Handler(cors.Options{
+			AllowedMethods: cfg.CORS.AllowMethods,
+			AllowedHeaders: cfg.CORS.AllowHeaders,
+			AllowedOrigins: cfg.CORS.AllowOrigins,
+			MaxAge:         cfg.CORS.MaxAge,
+		}))
+	}
+
+	r.Use(middleware.Logger, middleware.Recoverer, middleware.Timeout(cfg.RequestTimeout))
+
+	r.Get("/hell", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "heaven")
 	})
 
-	if cfg.RateLimitingConfig.Use {
-		r.Use(rl.RateLimitMiddleware)
+	r.Handle("/metrics", promhttp.Handler())
+
+	if cfg.WAF.Use {
+		waf, err := newWaf(cfg, logger)
+		if err != nil {
+			logger.Error("failed create new waf",
+				zap.Error(err))
+
+			return err
+		}
+		r.Handle("/*", txhttp.WrapHandler(waf, handler))
+
+	} else {
+		r.HandleFunc("/*", handler.ServeHTTP)
 	}
-
-	server := &http3.Server{
-		Addr:    cfg.Addr,
-		Handler: r,
-	}
-
-	return &Server{
-		router: r,
-		logger: logger,
-		cfg:    cfg,
-		h3S:    server,
-	}
-}
-
-func (s *Server) Run() error {
-	if err := s.h3S.ListenAndServe(); err != nil {
-		s.logger.Error().Msgf("failed listen and serve on %s: %v", s.cfg.Addr, err)
-
-		return err
-	}
-
 	return nil
 }
 
-func (s *Server) Shutdown(ctx context.Context) error {
-	if err := s.h3S.Shutdown(ctx); err != nil {
-		s.logger.Error().Msgf("failed shutdown server: %v", err)
-		return err
+func setupServer(cfg *config.Config, logger *logger.Logger) (Server, error) {
+	if cfg.Proto == HttpProto {
+		return NewHTTPServer(cfg, logger), nil
 	}
 
-	return nil
+	if cfg.Proto == Http3Proto {
+		return NewHTTP3Server(cfg, logger)
+	}
+
+	logger.Error("unknown proto type", zap.String("proto", cfg.Proto))
+
+	return nil, fmt.Errorf("unknown proto type: %s", cfg.Proto)
 }
